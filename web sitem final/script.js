6a997b07461504e9e64f9ac60301c19ca83eefdf
Bilalmuratpolat <146809647+Bilalmuratpolat@users.noreply.github.com>
2024-06-02 23:38:42 +0300

    tarım ürünleri web sitesi

---
+++ web sitem final/script.js
@@ -0,0 +1,8 @@
+// "Daha Fazla Bilgi" butonunu seçin
+var infoButton = document.querySelector('.buttons button');
+
+// Butona tıklandığında bir event dinleyicisi ekleyin
+infoButton.addEventListener('click', function() {
+    // Alert ile bir iletişim kutusu gösterin
+    alert('Daha fazla bilgi butonuna tıklandı!');
+});
